a647a97b2b9067ffc9641e1c2c602b37a18f3dca
Delong Zhai <delong.zhai@gmail.com>
2024-04-06 15:55:23 -0400

    setup meta data

--- setup.py
+++ setup.py
@@ -1,1 +1,6 @@
-__import__("setuptools").setup()
+__import__("setuptools").setup(
+    name="msgbridge",
+    version='0.2.0',
+    description="Plugins for handling messages between iframe and hosting website",
+    url = "https://github.com/ZDL-LLC/msgbridge"  # Add this line
+)

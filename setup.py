9b72418bac82f5b1e45521d230d1d548ea498439
Delong Zhai <delong.zhai@gmail.com>
2024-04-06 16:20:29 -0400

    change setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 __import__("setuptools").setup(
     name="msgbridge",
-    version='0.2.0',
+    version='0.2.5',
     description="Plugins for handling messages between iframe and hosting website",
-    url = "https://github.com/ZDL-LLC/msgbridge"  # Add this line
+    url="https://github.com/ZDL-LLC/msgbridge",  # Add this line
+    author="ZDL, LLC",
+    keywords="msgbridge, jupyterlite",
 )

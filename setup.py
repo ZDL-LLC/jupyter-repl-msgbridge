d14d1a3481a71f727e77999b595b24d1a57cdf96
Delong Zhai <delong.zhai@gmail.com>
2024-04-06 20:21:16 -0400

    try to fix publishing by remove urls from pyproject.toml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ __import__("setuptools").setup(
     name="msgbridge",
     version='0.2.5',
     description="Plugins for handling messages between iframe and hosting website",
-    url="https://github.com/ZDL-LLC/msgbridge",  # Add this line
+    url="https://github.com/ZDL-LLC/msgbridge",
     author="ZDL, LLC",
     keywords="msgbridge, jupyterlite",
 )
